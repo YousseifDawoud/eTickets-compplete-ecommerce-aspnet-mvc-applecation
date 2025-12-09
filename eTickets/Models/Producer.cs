a0400129049a4d6328e2329bd249fe168d63e413
YousseifDawoud <yousseifdawoud141@gmail.com>
2025-12-09 19:42:18 +0200

    Adding The Basic Modes Like [Actor,Producer,Cinema,Movie] And Create Enum(MovieCategory)

---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureURL { get; set; } = null!;
+
+        public string FullName { get; set; } = null!;
+
+        public string? Bio { get; set; }
+    }
+}
