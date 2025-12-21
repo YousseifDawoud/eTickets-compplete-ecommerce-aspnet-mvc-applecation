7dfccc96f1e8e1cea422b346835b2e0ad65dbb68
YousseifDawoud <yousseifdawoud141@gmail.com>
2025-12-21 13:24:25 +0200

    Add New Controller To GetActorById - Add New View To Show Actor Details-Handel iT From Service

--- eTickets/Controllers/ActorController.cs
+++ eTickets/Controllers/ActorController.cs
@@ -4,42 +4,50 @@ using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace eTickets.Controllers
+namespace eTickets.Controllers;
+
+public class ActorController : Controller
 {
-    public class ActorController: Controller
+    // Dependency Injection
+    private readonly IActorService _service;
+    public ActorController(IActorService service)
     {
-        // Dependency Injection
-        private readonly IActorService _service;
-        public ActorController(IActorService service)
-        {
-            _service = service;  
-        }
+        _service = service;
+    }
 
 
-        // GET: /Actor
-        public async Task<IActionResult> Index()
-        {
-            var actors = await _service.GetAllAsync(HttpContext.RequestAborted);
-            return View(actors);
-        }
+    // GET: /Actor
+    public async Task<IActionResult> Index()
+    {
+        var actors = await _service.GetAllAsync(HttpContext.RequestAborted);
+        return View(actors);
+    }
 
-        // Get: Actor/Create
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+    // Get: Actor/Create
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
 
-        // Post : Actor/Create
-        [HttpPost]
-        public async Task<IActionResult> Create(Actor actor)
+    // Post : Actor/Create
+    [HttpPost]
+    public async Task<IActionResult> Create(Actor actor)
+    {
+        if (!ModelState.IsValid)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(actor);
-            }
-            await _service.AddAsync(actor, HttpContext.RequestAborted);
-            return RedirectToAction(nameof(Index));
+            return View(actor);
         }
+        await _service.AddAsync(actor, HttpContext.RequestAborted);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // Get : Actor/Details/{id}
+    [HttpGet]
+    public async Task<IActionResult> Details(int id)
+    {
+        var actor = await _service.GetByIdAsync(id, HttpContext.RequestAborted);
+        if (actor is null) return  NotFound();
+        return View(actor);
     }
 }
